a293beff8f167aac8be2e4a25a381083abdc1f4d
KHMILA-Rania <rania.khmila@gmail.com>
2024-05-16 20:00:10 +0100

    tp4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/taskform/taskform.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    NavbarComponent,
+   
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
