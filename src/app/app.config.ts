266128c6378017b9deb63167d00e470f3909321b
KHMILA-Rania <rania.khmila@gmail.com>
2024-05-22 16:29:24 +0100

    tp5 done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { provideHttpClient } from
+'@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),
+     provideHttpClient(),RouterModule]
 };
